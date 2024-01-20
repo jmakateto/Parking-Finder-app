405e40db0e290bb2b62c4db7cedacb15d17067bc
Esther <est.karanja@gmail.com>
2024-01-20 21:28:55 +0300

    made the navbar responsive, added routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
-import './App.css'
+import './styles.css'
 import Home from './Components/Home';
+import { Route, Routes } from 'react-router-dom';
+import Login from './Components/Login';
+import Navbar from './Components/Navbar';
+import SignUp from './Components/SignUp';
+import HowItWorks from './Components/HowItWorks';
+import Reviews  from './Components/Reviews';
+import AdminPanel from './Components/AdminPanel';
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/how-it-works" element={<HowItWorks/>}/>
+        <Route path="/reviews" element={<Reviews/>}/>
+        <Route path="/sign-up" element={<SignUp/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/admin-panel" element={<AdminPanel/>}/>
+      </Routes>
+  
     </div>
   );
 }
