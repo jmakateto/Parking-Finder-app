1c9dfdc065530888bdc420336bf49369f68b6e8a
Abigail <abby.a.otieno@gmail.com>
2024-01-22 11:37:16 +0300

    Created main.py

---
+++ server/main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from flask import Flask
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///parking.db'
+  
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
